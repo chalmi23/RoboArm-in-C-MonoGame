9b0c0718b1bb925a8e6d9f9d287c6e90392784b0
chalmi23 <michalekxdpl23@gmail.com>
2023-10-22 17:51:15 +0200

    test

--- RoboArm/Game1.cs
+++ RoboArm/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Security.Cryptography;
 
 namespace RoboArm
 {
@@ -18,6 +19,9 @@ namespace RoboArm
 
         private float zoom = 1.0f;
         private float zoomSpeed = 0.02f;
+
+        private float time = 0; // Czas w grze
+        private float angleX = 0.0f, angleY = 0.0f;
 
         public Game1()
         {
@@ -43,14 +47,33 @@ namespace RoboArm
             metal2Texture = Content.Load<Texture2D>("metal2");
             metal3Texture = Content.Load<Texture2D>("metal3");
 
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.VertexColorEnabled = true;
         }
 
         protected override void Update(GameTime gameTime)
         {
+            KeyboardState keyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (keyboardState.IsKeyDown(Keys.Right)) angleY += 0.02f;
+            if (keyboardState.IsKeyDown(Keys.Left)) angleY -= 0.02f;
+
+            if (keyboardState.IsKeyDown(Keys.Up)) angleX += 0.02f;
+            if (keyboardState.IsKeyDown(Keys.Down)) angleX -= 0.02f;
+
+            if (keyboardState.IsKeyDown(Keys.Q)) zoom += zoomSpeed;
+
+            world = Matrix.Identity;
+            view = Matrix.CreateLookAt(new Vector3(0.0f, 0.0f, 6.0f / zoom), Vector3.Zero, Vector3.Up);
+            view = Matrix.CreateRotationX(angleX) * Matrix.CreateRotationY(angleY) * view;
+            proj = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(50), GraphicsDevice.Viewport.AspectRatio, 0.01f, 1000.0f);
+
+            basicEffect.World = world;
+            basicEffect.View = view;
+            basicEffect.Projection = proj;
 
             base.Update(gameTime);
         }
